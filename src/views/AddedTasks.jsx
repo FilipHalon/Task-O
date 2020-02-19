0d7f12bca0b30cd1d2e981566bd780f240239488
FilipHalon <halon.filip@gmail.com>
2020-02-19 10:02:13 +0100

    Task/Solution Add (Content Add) Page added; minor fixes.

--- src/views/AddedTasks.jsx
+++ src/views/AddedTasks.jsx
@@ -6,26 +6,26 @@ export default props => {
     const isThisProfileTask = props.ownTasks === "thisProfileTasks"; 
 
     return(
-        <main>
+        <React.Fragment>
             <Sidebar />
             <section className="task-description">
-            <div className="task-description">
-                {!isThisProfileTask &&
-                <div className="task-general-info">
-                    <div>
-                        <h2>Zadania</h2>
-                        <h2>Aktywne: 100</h2>
-                        {/* <p>Firmy: 100</p> */}
+                <div className="task-description">
+                    {!isThisProfileTask &&
+                    <div className="task-general-info">
+                        <div>
+                            <h2>Zadania</h2>
+                            <h2>Aktywne: 100</h2>
+                            {/* <p>Firmy: 100</p> */}
+                        </div>
+                    </div>
+                    }
+                    <div className="task-area">
+                        <AddedTask thisProfileTasks={isThisProfileTask && "thisProfileTasks"}/>
+                        <AddedTask thisProfileTasks={isThisProfileTask && "thisProfileTasks"}/>
+                        <AddedTask thisProfileTasks={isThisProfileTask && "thisProfileTasks"}/>
                     </div>
                 </div>
-                }
-                <div className="task-area">
-                    <AddedTask thisProfileTasks={isThisProfileTask && "thisProfileTasks"}/>
-                    <AddedTask thisProfileTasks={isThisProfileTask && "thisProfileTasks"}/>
-                    <AddedTask thisProfileTasks={isThisProfileTask && "thisProfileTasks"}/>
-                </div>
-            </div>
-        </section>
-        </main>
+            </section>
+        </React.Fragment>
     )
 };
---
+++ src/views/ContentAddForm.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLightbulb, faCloudUploadAlt } from '@fortawesome/free-solid-svg-icons';
+import Sidebar from "../components/Sidebar";
+
+export default props => {
+    const isNewSolution = props.addType === "solution";
+
+    return(
+        <div className="solution-add">
+            <Sidebar />
+            <section className="task-description">
+                <div className="task-description">
+                    <div className="task-border">
+                        <div className="task-description-heading">
+                            <FontAwesomeIcon icon={faLightbulb} />
+                            <span>Dodawanie {isNewSolution ? "rozwiązania" : "zadania"}</span>
+                        </div>
+                        <form className="solution-add-area">
+                            {isNewSolution ?
+                            <div className="input-area">
+                                <input type="file"/>
+                            </div>
+                            :
+                            <div className="input-area">
+                                <textarea type="text" rows="4" cols="90" placeholder="Podaj streszczenie zadania"/>
+                                <input type="text" placeholder="Wybierz język programowania"/>
+                                <textarea type="text" rows="8" cols="90" placeholder="Podaj szczegóły zadania"/>
+                            </div>
+                            }
+                            <div>
+                                <button type="submit"><FontAwesomeIcon icon={faCloudUploadAlt} /> Publikuj</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+};
