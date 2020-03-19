d2c16112a75577297c6bd2960ea44421a4b6cc20
FilipHalon <halon.filip@gmail.com>
2020-03-19 14:43:28 +0100

    Updates

---
+++ precache-manifest.6ed7b8596c68f44190b2a846fba9bd80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c93207a5423425c45daef00daaddd0b",
+    "url": "/task-o/index.html"
+  },
+  {
+    "revision": "b97468f5516a8a1e1612",
+    "url": "/task-o/static/css/2.1a1a1556.chunk.css"
+  },
+  {
+    "revision": "4ae9e024690ba271d9f3",
+    "url": "/task-o/static/css/main.0ccfd92f.chunk.css"
+  },
+  {
+    "revision": "b97468f5516a8a1e1612",
+    "url": "/task-o/static/js/2.c20ccf72.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/task-o/static/js/2.c20ccf72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ae9e024690ba271d9f3",
+    "url": "/task-o/static/js/main.216920c0.chunk.js"
+  },
+  {
+    "revision": "f9bad64860cbd3904358",
+    "url": "/task-o/static/js/runtime-main.849d92cd.js"
+  },
+  {
+    "revision": "753a0635447abc5800666ac3ab1546ef",
+    "url": "/task-o/static/media/taskoala.753a0635.png"
+  }
+]);
