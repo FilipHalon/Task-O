e8bee268c4ec071c27d55c8c19c8066a051333e0
FilipHalon <halon.filip@gmail.com>
2020-03-19 14:47:57 +0100

    Updates

---
+++ precache-manifest.7c33b1e8fe9c74062c35a4ae3c09d64a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "463881733634e3f0f73200f4062bf117",
+    "url": "/Task-O/index.html"
+  },
+  {
+    "revision": "996e66695a42afb99fa8",
+    "url": "/Task-O/static/css/2.1a1a1556.chunk.css"
+  },
+  {
+    "revision": "4ae9e024690ba271d9f3",
+    "url": "/Task-O/static/css/main.0ccfd92f.chunk.css"
+  },
+  {
+    "revision": "996e66695a42afb99fa8",
+    "url": "/Task-O/static/js/2.3cba49f8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Task-O/static/js/2.3cba49f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ae9e024690ba271d9f3",
+    "url": "/Task-O/static/js/main.216920c0.chunk.js"
+  },
+  {
+    "revision": "759637c8f1d9d63823f9",
+    "url": "/Task-O/static/js/runtime-main.5966e394.js"
+  },
+  {
+    "revision": "753a0635447abc5800666ac3ab1546ef",
+    "url": "/Task-O/static/media/taskoala.753a0635.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/task-o/precache-manifest.6ed7b8596c68f44190b2a846fba9bd80.js"
+  "/Task-O/precache-manifest.7c33b1e8fe9c74062c35a4ae3c09d64a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/task-o/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Task-O/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
