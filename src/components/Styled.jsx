282cf6d4bb7cb9a04a8a64259e995dbd126bb3c5
FilipHalon <halon.filip@gmail.com>
2020-03-26 18:48:48 +0100

    Styled removed from Components.

--- src/components/Styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const LoggedMain = styled.main`
-    display: grid;
-    grid: auto / 3fr 7fr;
-    background: #E6EBF1;
-`;
-
-export const UnloggedMain = styled.main``
